76578ce4b096dbceaa2e74f429ff43ec2d976ad6
Ezzatt Allah <ezzatt145@gmail.com>
2025-01-28 14:22:22 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import '@fortawesome/fontawesome-free/js/all.min';
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
 
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("أُضيف المنتج إلى عربة الشراء")
+    })
+})
+
+
 console.log("أهلًا بكم في متجر عربي");
 
 console.log("أهلًا بكم في أكاديمية حسوب");
-
-
